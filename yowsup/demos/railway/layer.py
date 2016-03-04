d90fac3dba3f317a70bde455f605554c9a2442e0
Shivam Rana <shivamrana@MacBook-Pro.local>
2016-03-05 04:03:48 +0530

    Added availability support

--- yowsup/demos/railway/layer.py
+++ yowsup/demos/railway/layer.py
@@ -1,5 +1,6 @@
 from yowsup.layers.interface  import YowInterfaceLayer, ProtocolEntityCallback
 import pnr_worker
+import availability_worker
 
 class RailwayLayer(YowInterfaceLayer):
 
@@ -11,6 +12,10 @@ class RailwayLayer(YowInterfaceLayer):
             if text[:3] == "pnr":
                 print("Echoing PNR to %s" % (messageProtocolEntity.getFrom(False)))
                 messageProtocolEntity.setBody(pnr_worker.get_pnr(text[4:]))
+            if text[:6] == "status":
+                query = text.split(" ")
+                print("Echoing availability to %s" % (messageProtocolEntity.getFrom(False)))
+                messageProtocolEntity.setBody(availability_worker.get_availability(query[1], query[2], query[3], query[4]))
 
         self.toLower(messageProtocolEntity.forward(messageProtocolEntity.getFrom()))
         self.toLower(messageProtocolEntity.ack())
